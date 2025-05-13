c749de80ef9c499446c2ad0b5892e53b935f1445
BartB25 <beeckmansb@gmail.com>
2025-05-13 16:31:20 +0200

    carousel

--- mijnJavaScript.js
+++ mijnJavaScript.js
@@ -1,4 +1,22 @@
+// menu open en dichtklappen
 function toggleMenu() {
     const navMenu = document.querySelector('.nav-menu');
     navMenu.classList.toggle('show');
+}
+
+//doorscrollen door carousel 
+let scrollInterval;
+
+function startScrolling(direction) {
+    const track = document.getElementById("carouselTrack");
+    scrollInterval = setInterval(() => {
+        track.scrollBy({
+            left: direction * 200, // kleine stapjes voor soepel scrollen
+            behavior: 'auto'
+        });
+    }, 5); // elke 5ms
+}
+
+function stopScrolling() {
+    clearInterval(scrollInterval);
 }
